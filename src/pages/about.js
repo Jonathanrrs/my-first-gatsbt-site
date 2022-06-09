dad391cee6225e9ba1ab3df4c7dce52361ce2dab
jonathan <yoni_12rrs@hotmail.com>
2022-06-09 16:49:40 -0500

    implementation of module css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,14 @@
 import { Link } from "gatsby";
 import React from "react";
+import Layout from "../../components/layout";
 
 const AboutPage = () => {
   return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <Link to="/">Home</Link>
+    <Layout pageTitle="About me">
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
       </p>
-    </main>
+    </Layout>
   );
 };
 
