2d70018d904e61f0d1c46d6974b084e140fe54bd
jonathan <yoni_12rrs@hotmail.com>
2022-06-09 16:25:05 -0500

    Navigate to page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import { Link } from "gatsby";
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <Link to="/">Home</Link>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </main>
+  );
+};
+
+export default AboutPage;
