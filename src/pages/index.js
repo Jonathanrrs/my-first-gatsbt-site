4d416b07a439882a64acc2158f5eb8aecaf3f945
jonathan <yoni_12rrs@hotmail.com>
2022-06-09 17:16:33 -0500

    use a package and its configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
-import { Link } from 'gatsby';
-import React from 'react'
-import Layout from '../../components/layout';
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+import Layout from "../../components/layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
-  )
-}
+  );
+};
 
 /* It's important make this way because otherwise it will not working */
 export default IndexPage;
