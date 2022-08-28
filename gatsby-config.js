cb0d74f8752dbdd0ea8947d3758431deb0dff04b
jonathan <yoni_12rrs@hotmail.com>
2022-08-28 13:24:41 -0500

    funcionará aún así el ssr?

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,23 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-react-helmet-async",
-    "gatsby-plugin-netlify"
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/*": [
+            "cache-control: public, max-age=0, must-revalidate"
+          ],
+          "/static": [
+              "cache-control: public, max-age=31536000, immutable"
+          ],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        // mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 };
